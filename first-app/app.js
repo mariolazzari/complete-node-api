9a6aedcf14b37c23542ee4d3ea8becead35802ba
mariolazzari <mario.lazzari@gmail.com>
2019-04-14 21:47:23 +0200

    Done chapter 2

--- first-app/app.js
+++ first-app/app.js
@@ -7,7 +7,63 @@ hello("Mario");
 console.log(module);
 
 // import module
-const logger = require("./logger");
-
-console.log(logger);
+const Logger = require("./logger");
+const logger = new Logger();
+logger.on("messageLogged", e => {
+  console.log("Logger class event", e);
+});
 logger.log("test log");
+
+// path module
+const path = require("path");
+const pathObj = path.parse(__filename);
+console.log(pathObj);
+
+// os module
+const os = require("os");
+const totalMem = os.totalmem();
+const freeMem = os.freemem();
+
+console.log(`Total memory: ${totalMem}
+Free  memory: ${freeMem}`);
+
+// fs module
+const fs = require("fs");
+// sync version
+const files = fs.readdirSync("./");
+console.log("Sync files:", files);
+// async version
+fs.readdir("./", (err, files) => {
+  if (err) return console.log(err);
+  console.log("Async files", files);
+});
+
+// events
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+// subscribe event: on = addListener
+emitter.on("messageLogged", () => {
+  console.log("Listener called");
+});
+// raise an event
+emitter.emit("messageLogged");
+// event arguments
+emitter.on("messageArgs", e => {
+  console.log("Event args:", e);
+});
+emitter.emit("messageArgs", { id: 1, message: "Error logger 1" });
+
+// http module
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello world!");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+server.listen(3000);
+console.log("Server started...");
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,9 +1,13 @@
+const EventEmitter = require("events");
+
 let url = "http://mylogger.io/log";
 
-const log = message => {
-  console.log(message);
-};
+class Logger extends EventEmitter {
+  log(message) {
+    console.log(message);
+    // raise event
+    this.emit("messageLogged", { id: 1, url });
+  }
+}
 
-module.exports = {
-  log
-};
+module.exports = Logger;
