357add86e2240719014e475b2847611181842280
mariolazzari <mario.lazzari@gmail.com>
2019-04-25 11:44:55 +0200

    Done chapter 4

--- express-demo/index.js
+++ express-demo/index.js
@@ -23,20 +23,14 @@ app.get("/api/courses/:id", (req, res) => {
   if (course) {
     res.send(course);
   } else {
-    res.status(200).send("Course not found.");
+    res.status(404).send("Course not found.");
   }
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -47,16 +41,47 @@ app.post("/api/courses", (req, res) => {
   res.send([...courses, course]);
 });
 
-/*
 app.put("/api/courses/:id", (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (course) {
-      res.send(course);
-    } else {
-      res.status(200).send("Course not found.");
-    }
-  });
-  */
+  // check if course exists
+  const id = parseInt(req.params.id);
+  const course = courses.find(c => c.id === id);
+  if (!course) {
+    res.status(404).send("Course not found.");
+    return;
+  }
+
+  // validate update
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // update course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  // check if course exists
+  const id = parseInt(req.params.id);
+  const course = courses.find(c => c.id === id);
+  if (!course) return res.status(404).send("Course not found.");
+
+  const idx = courses.indexOf(course);
+  courses.splice(idx, 1);
+  res.send(course);
+});
+
+// course validation
+const validateCourse = course => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+};
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express stated on port ${PORT}`));
